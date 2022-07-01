8c6ba7335e4c21ef210293a55e95f1f5d60b7f10
ovclid <53159064+ovclid@users.noreply.github.com>
2022-07-01 21:30:44 +0900

    Update pandas_test.py

--- pandas/pandas_test.py
+++ pandas/pandas_test.py
@@ -9,7 +9,7 @@ data = [ [f.name()] + [random.randint(1, 100) for i in range(5)] for j in range(
 df = pd.DataFrame(data, columns = ["이름", "국어", "영어", "수학", "사회", "정보"])
 
 df.to_csv("score.csv")
-df.to_excel("score.xlsx")
+df.to_excel("score.xlsx", index = False)
 
 #%%
 df = pd.read_csv("https://raw.githubusercontent.com/ovclid/leture/main/pandas/score.csv", index_col = 0)
