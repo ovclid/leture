1e1613fef4a0a8a20ce74cae037a32ad262afc90
ovclid <53159064+ovclid@users.noreply.github.com>
2022-07-01 21:41:18 +0900

    Update pandas_test.py

--- pandas/pandas_test.py
+++ pandas/pandas_test.py
@@ -13,3 +13,23 @@ df.to_excel("score.xlsx", index = False)
 
 #%%
 df = pd.read_csv("https://raw.githubusercontent.com/ovclid/leture/main/pandas/score.csv", index_col = 0)
+# %%
+import requests
+import pandas as pd
+url = 'https://github.com/ovclid/leture/blob/main/pandas/score.xlsx?raw=true'
+r = requests.get(url)
+
+
+with open('temp.xlsx', 'wb') as f:
+    f.write(r.content)
+
+    
+"""
+f = open('temp.xlsx', 'wb')
+f.write(r.content)
+f.close()
+"""
+
+df = pd.read_excel('temp.xlsx')
+
+# %%
