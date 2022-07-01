1a18c2486a034dc20be62462494a2554a6696fba
ovclid <53159064+ovclid@users.noreply.github.com>
2022-07-02 07:06:22 +0900

    Update pandas_test.py

--- pandas/pandas_test.py
+++ pandas/pandas_test.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import faker
 import random
-
+#%%
 f = faker.Faker("ko-kr")
 data = [ [f.name()] + [random.randint(1, 100) for i in range(5)] for j in range(100)]
 
@@ -13,6 +13,35 @@ df.to_excel("score.xlsx", index = False)
 
 #%%
 df = pd.read_csv("https://raw.githubusercontent.com/ovclid/leture/main/pandas/score.csv", index_col = 0)
+
+for i in df.columns[1:6]:
+    print(i)
+    for j in range(len(df)):
+        grade = ""
+        if df.loc[j, i] >= 90: grade = "수"
+        elif df.loc[j, i] >= 80: grade = "우"
+        elif df.loc[j, i] >= 70: grade = "미"
+        elif df.loc[j, i] >= 60: grade = "양"
+        else : grade = "가"
+
+        df.loc[j, i+"_등급"] = grade
+
+df["국어_등급"].value_counts().sort_index()
+
+
+gi = ["수", "우", "미", "양", "가"]
+df["국어_등급"].value_counts().reindex(gi)
+
+#%%
+import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
+rc('font', family=font_name)
+
+
+df["국어_등급"].value_counts().reindex(gi).plot()
+df["국어_등급"].value_counts().reindex(gi).plot(kind="bar")
+
 # %%
 import requests
 import pandas as pd
