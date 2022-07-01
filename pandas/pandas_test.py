563fbc8649e25081981d5423eee673cd7f323193
ovclid <53159064+ovclid@users.noreply.github.com>
2022-07-01 21:27:34 +0900

    Create pandas_test.py

---
+++ pandas/pandas_test.py
@@ -0,0 +1,15 @@
+#%%
+import pandas as pd
+import faker
+import random
+
+f = faker.Faker("ko-kr")
+data = [ [f.name()] + [random.randint(1, 100) for i in range(5)] for j in range(100)]
+
+df = pd.DataFrame(data, columns = ["이름", "국어", "영어", "수학", "사회", "정보"])
+
+df.to_csv("score.csv")
+df.to_excel("score.xlsx")
+
+#%%
+df = pd.read_csv("https://raw.githubusercontent.com/ovclid/leture/main/pandas/score.csv", index_col = 0)
